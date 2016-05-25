e4f6f82133b77ae80d86f96d6bd7d45f62bbfb21
Katya <kolikova@gmail.com>
2016-05-25 16:03:07 -0700

    data management and first plots

---
+++ data_manag&visualization.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
+
+red = pd.read_csv('winequality-red.csv', low_memory=False, sep=';')
+white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
+
+
+def call (functionToCall):
+    print('Red')
+    functionToCall(red)
+    print('\n')
+
+    print('White')
+    functionToCall(white)
+    print('\n')
+
+# # ------------- to print basic info about loaded dataset ----------------
+def basicInfo(wine_set):
+    print(len(wine_set))
+    print(len(wine_set.columns))
+    print(list(wine_set.columns.values))
+    print(wine_set.ix[:10,:4])
+
+call(basicInfo)
+
+# ------------ to print frequency distributions of wines' quality ------
+def frequencyDists(wine_set):
+    print("This is the frequency distribution of the wines' quality.")
+    print(wine_set.groupby("quality").size()*100 / len(wine_set))
+    print()
+
+call(frequencyDists)
+
+# ------------- to print quartile split of the quality variable -----------------
+def quartileSplit(wine_set):
+    print("This is the quartile split of the wines' quality. I-st column contains the intervals of wines' quality;")
+    print("II-nd - the number of wine samples with the quality in the corresponding interval.")
+    wine_set["quality_quart"] = pd.qcut(wine_set["quality"], 3)
+    print(wine_set.groupby("quality_quart").size())
+
+call(quartileSplit)
+
+# # -------------- to check for missing data in all columns ------------------------
+# print(pd.isnull(red).any())
+# print(pd.isnull(white).any())
+
+
+# ---------------- visualization ----------------------------
+def countplots(wine_set):
+    wine_set["quality"] = pd.Categorical(wine_set["quality"])
+    seaborn.countplot(x="quality", data=wine_set)
+    plt.xlabel("Quality level of wine (0-10 scale)")
+    plt.show()
+
+call(countplots)
