7098db22ab4bffd890e7dd1de06866bcaf52dad8
Katya <kolikova@gmail.com>
2016-05-31 16:48:12 -0700

    data analysis - anova

---
+++ data_analysis.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import statsmodels.formula.api as smf
+import statsmodels.stats.multicomp as multi
+
+red = pd.read_csv('winequality-red.csv', low_memory=False, sep=';')
+white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
+
+
+def call(functionToCall):
+    print('Red')
+    functionToCall(red)
+    print('\n')
+
+    print('White')
+    functionToCall(white)
+    print('\n')
+
+
+# ----- to remove all spaces from column names ---------
+def remove_col_spaces(wine_set):
+    wine_set.columns = [x.strip().replace(' ', '_') for x in wine_set.columns]
+    return wine_set
+
+call(remove_col_spaces)
+
+# --------- to add a column 'quality_mark' which is categorical representation of the quality-----------
+def add_categ_quality(wine_set):
+    low = wine_set[wine_set['quality'] <= 5]
+    medium = wine_set[(wine_set['quality'] == 6) | (wine_set['quality'] == 7)]
+    high = wine_set[wine_set['quality'] > 7]
+
+    low['quality_mark'] = 'low'
+    medium['quality_mark'] = 'medium'
+    high['quality_mark'] = 'high'
+
+    frames = [low, medium, high]
+    return pd.concat(frames)
+
+
+# # ------------- Hypothesis Testing and ANOVA ------------------------------------------------------------
+
+# # ------------- to use ols function for calculating the F-statistic and associated p-value ---------------
+def anova(wine_set):
+    prepared_data = add_categ_quality(wine_set)
+    model1 = smf.ols(formula="total_sulfur_dioxide ~ C(quality_mark)", data=prepared_data)
+    results1 = model1.fit()
+    print(results1.summary())
+    #
+    sub = prepared_data[['total_sulfur_dioxide', 'quality_mark']]
+    print("\nMeans for total sulfur dioxide by quality marks of wine")
+    print(sub.groupby('quality_mark').mean())
+    print("\nStandard deviations for total sulfur dioxide by quality marks of wine")
+    print(sub.groupby('quality_mark').std(), '\n')
+    #
+    # # ------------- to perform Post hoc test (using Tukey's Honestly Significant Difference Test) -------------------------
+    mc1 = multi.MultiComparison(sub['total_sulfur_dioxide'], sub['quality_mark'])
+    res1 = mc1.tukeyhsd()
+    print(res1.summary())
+
+print('----------------Hypothesis Testing and ANOVA------------------------')
+call(anova)
