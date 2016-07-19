7dad7982821ea3583cebc6a55cccd92fe2564272
Katya <kolikova@gmail.com>
2016-07-18 20:23:16 -0700

    machine learning - lasso

--- machine_learning.py
+++ machine_learning.py
@@ -5,6 +5,11 @@ import sklearn
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 import matplotlib.pyplot as plt
 import operator
+from sklearn import preprocessing
+from sklearn.linear_model import LassoLarsCV
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
 
 red = pd.read_csv('winequality-red.csv', low_memory=False, sep=';')
 white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
@@ -123,5 +128,65 @@ def random_forests(wine_set):
 
     print(accuracy)
 
-print('----------------Random Forests------------------------')
-call(random_forests)
+# print('----------------Random Forests------------------------')
+# call(random_forests)
+
+
+#________________________________Lasso Regression__________________________________
+def lasso_regr(wine_set):
+
+    pred = wine_set[["density", 'alcohol', 'sulphates', 'pH', 'volatile_acidity', 'chlorides', 'fixed_acidity',
+                           'citric_acid', 'residual_sugar', 'free_sulfur_dioxide', 'total_sulfur_dioxide']]
+    predictors = pred.copy()
+    targets = wine_set.quality
+
+    # standardize predictors to have mean=0 and sd=1
+    predictors = pd.DataFrame(preprocessing.scale(predictors))
+    predictors.columns = pred.columns
+    # print(predictors.head())
+
+    # split into training and testing sets
+    pred_train, pred_test, tar_train, tar_test = train_test_split(predictors, targets, test_size=.3, random_state=123)
+
+    # specify the lasso regression model
+    model = LassoLarsCV(cv=10, precompute=False).fit(pred_train, tar_train)
+
+    print('Predictors and their regression coefficients:')
+    d = dict(zip(predictors.columns, model.coef_))
+    for k in d:
+        print(k, ':', d[k])
+
+    # plot coefficient progression
+    m_log_alphas = -np.log10(model.alphas_)
+    # ax = plt.gca()
+    plt.plot(m_log_alphas, model.coef_path_.T)
+    print('\nAlpha:', model.alpha_)
+    plt.axvline(-np.log10(model.alpha_), linestyle="dashed", color='k', label='alpha CV')
+    plt.ylabel("Regression coefficients")
+    plt.xlabel("-log(alpha)")
+    plt.title('Regression coefficients progression for Lasso paths')
+    plt.show()
+
+    # plot mean squared error for each fold
+    m_log_alphascv = -np.log10(model.cv_alphas_)
+    plt.plot(m_log_alphascv, model.cv_mse_path_, ':')
+    plt.plot(m_log_alphascv, model.cv_mse_path_.mean(axis=-1), 'k', label='Average across the folds', linewidth=2)
+    plt.legend()
+    plt.xlabel('-log(alpha)')
+    plt.ylabel('Mean squared error')
+    plt.title('Mean squared error on each fold')
+    plt.show()
+
+    # Mean squared error from training and test data
+    train_error = mean_squared_error(tar_train, model.predict(pred_train))
+    test_error = mean_squared_error(tar_test, model.predict(pred_test))
+    print('\nMean squared error for training data:', train_error)
+    print('Mean squared error for test data:', test_error)
+
+    rsquared_train = model.score(pred_train, tar_train)
+    rsquared_test = model.score(pred_test, tar_test)
+    print('\nR-square for training data:', rsquared_train)
+    print('R-square for test data:', rsquared_test)
+
+print('----------------Lasso Regression------------------------')
+call(lasso_regr)
